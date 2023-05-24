bf1591c2a2e16009403d6bf5afdf590cd494f258
thasup <thanachonfirst@hotmail.com>
2023-05-25 00:07:57 +0700

    Persisting message across requests

--- chatbot.py
+++ chatbot.py
@@ -6,28 +6,38 @@ config = dotenv_values(".env")
 
 openai.api_key  = config["OPENAI_API_KEY"]
 
-def get_and_render_prompt_chat(text):
-    messages = [
-        {
-            "role": "system",
-            "content": "You're my bot assistant."
-        },
+messages = []
+messages.append(
+    {
+        "role": "system",
+        "content": "You're my bot assistant."
+    }
+)
+
+def ask_chatgpt():
+  completion = openai.ChatCompletion.create(
+          model="gpt-3.5-turbo",
+          messages=messages,
+          max_tokens=200,
+          top_p=0.5,
+          frequency_penalty=0,
+          presence_penalty=0
+      )
+  messages.append(completion["choices"][0]["message"].to_dict())
+  print(f"ChatBot: {completion.choices[0].message.content}")
+
+
+while True:
+  try:
+    user_input = input("You: ")
+    messages.append(
         {
             "role": "user",
-            "content": f"{text}"
-        },
-    ]
-
-    completion = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
-            messages=messages,
-            max_tokens=200,
-            top_p=0.5,
-            frequency_penalty=0,
-            presence_penalty=0
-        )
-    print(completion.choices[0].message.content)
-    print("-----")
-    print(completion)
-
-get_and_render_prompt_chat("Tell a short inspiration story.")
+            "content": f"{user_input}"
+        }
+    )
+    ask_chatgpt()
+  except KeyboardInterrupt:
+    print("Exiting...")
+    messages = []
+    break
