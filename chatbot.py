df013c9d1d9c797f3907239355c95e24e6a2c76a
thasup <thanachonfirst@hotmail.com>
2023-05-25 01:43:32 +0700

    Colorizing the chatbot output

--- chatbot.py
+++ chatbot.py
@@ -6,6 +6,21 @@ config = dotenv_values(".env")
 
 openai.api_key  = config["OPENAI_API_KEY"]
 
+def bold(text):
+  bold_start = "\033[1m"
+  bold_end = "\033[0m"
+  return bold_start + text + bold_end
+
+def blue(text):
+  blue_start = "\033[34m"
+  blue_end = "\033[0m"
+  return blue_start + text + blue_end
+
+def red(text):
+  red_start = "\033[31m"
+  red_end = "\033[0m"
+  return red_start + text + red_end
+
 def main():
   parser = argparse.ArgumentParser(description="Simple command line chatbot with GPT-3.5")
   parser.add_argument(
@@ -35,10 +50,11 @@ def main():
             stream=True
         )
     messages.append(completion["choices"][0]["message"].to_dict())
+    print(bold(red(f"Assistant:")), completion.choices[0].message.content)
 
   while True:
     try:
-      user_input = input("You: ")
+      user_input = input(bold(blue("You: ")))
       messages.append(
           {
               "role": "user",
